419840a70be5a17c6274b14b09fd6f896c4d78d6
pawan kumar <131910770+pawansiddh@users.noreply.github.com>
2026-08-20 21:31:35 +0530

    Redesign login as personal creator story

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='study-tracker-v5';
+const CACHE='study-tracker-v6';
 const ASSETS=['./','./index.html','./styles.css','./enhancements.css','./jobs.js','./app.js','./config.js','./manifest.webmanifest','./pawan-kumar-siddh.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
