c38b81ea9d0566851a1db1918ca3b2bbbb314345
pawan kumar <131910770+pawansiddh@users.noreply.github.com>
2026-08-20 21:24:05 +0530

    Polish calendar modals settings and login

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='study-tracker-v4';const ASSETS=['./','./index.html','./styles.css','./enhancements.css','./jobs.js','./app.js','./config.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
+const CACHE='study-tracker-v5';
+const ASSETS=['./','./index.html','./styles.css','./enhancements.css','./jobs.js','./app.js','./config.js','./manifest.webmanifest','./pawan-kumar-siddh.jpg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
