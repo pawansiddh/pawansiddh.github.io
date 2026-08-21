1f96729508337f5c9eb70f432d4143c0a33b90dc
pawan kumar <131910770+pawansiddh@users.noreply.github.com>
2026-08-21 12:18:21 +0530

    Block cached cloud profiles after account removal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='study-tracker-v19';
+const CACHE='study-tracker-v22';
 const ASSETS=['./','./index.html','./styles.css','./enhancements.css','./jobs.js','./family.js','./app.js','./config.js','./manifest.webmanifest','./pawan-kumar-siddh.jpg','./study-tracker-job-capture.zip'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
